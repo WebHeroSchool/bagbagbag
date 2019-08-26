06b23bd17edb2cc2d332ee9d0fcea2703c903944
Никита Родионов <nikita.rodionov.job@yandex.ru>
2019-08-26 12:04:01 +0300

    fix mistakes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
+const config = {
+  classNameOfMainForThreeCards: 'main-for-three-cards',
+  classNameOfMainForSixCards: 'main-for-six-cards',
+  classNameOfMainForTenCards: 'main-for-ten-cards',
+  classNameOfMenuBeforeStart: 'nav-before-start',
+  classNameOfAllCards: 'card',
+  classNameOfCards: 'card card__looser',
+  classNameOfLuserCards: 'card__looser',
+  classNameOfFrontSideForCard: 'card__front',
+  classNameOfBackSideForCard: 'card__looser_back',
+  classNameOfLoserBack: 'card__looser_back',
+  classNameOfWinnerBack: 'card__winner_back',
+  classNameOfCardLooserRotated: 'card__looser_rotated',
+  classNameOfCardWinnerRotated: 'card__winner_rotated',
+  classNameOfRandomWinnerCard: 'card card__winner',
+  idNameOfMenu: 'nav',
+  idNameOfMain: 'main',
+  idNameOfButtonStartGame: 'start-game',
+  diff: 'diff',
+  cardWin: 'card-win',
+}
+
+
 let numberOfCard;
 let cards = [];
-const button = document.getElementById('start-game');
+const button = document.getElementById(config.idNameOfButtonStartGame);
 
 function getDifficultyOfGame() {
-  const diff = document.getElementsByName("diff");
+  const diff = document.getElementsByName(config.diff);
   if (diff[0].checked) {
     return (numberOfCard = 3);
   } else if (diff[1].checked) {
@@ -11,46 +34,45 @@ function getDifficultyOfGame() {
   } else if (diff[2].checked) {
     return (numberOfCard = 10);
   } else {
-   return (numberOfCard = 3);
+    return (numberOfCard = 3);
   }
 }
 
 const createCardsAndshowCards = () => {
   getDifficultyOfGame();
   createMainInHtml();
+  const main = document.getElementById(config.idNameOfMain);
 
   if (numberOfCard == 3) {
-    createCards(3, 'card__looser card__easy');
-    addFrontAndBackSidesToCard('.card__easy');
+    createCards(3);
+    addFrontAndBackSidesToCards();
+    main.className = config.classNameOfMainForThreeCards;
   } else if (numberOfCard == 6) {
-    createCards(6, 'card__looser card__medium');
-    addFrontAndBackSidesToCard('.card__medium');
-    const mainById = document.getElementById('main');
-    mainById.style.width = '800px';
+    createCards(6);
+    addFrontAndBackSidesToCards();
+    main.className = config.classNameOfMainForSixCards;
   } else {
-    createCards(10, 'card__looser card__hard');
-    addFrontAndBackSidesToCard('.card__hard');
-    const mainById = document.getElementById('main');
-    mainById.style.width = '1210px';
+    createCards(10);
+    addFrontAndBackSidesToCards();
+    main.className = config.classNameOfMainForTenCards;
   }
 }
 
 
 const startGameAndDecideWinnerAndLoosers = () => {
-  const menu = document.getElementById('nav');
+  const menu = document.getElementById(config.idNameOfMenu);
+  menu.className = config.classNameOfMenuBeforeStart;
   const body = document.body;
-  nav.style.display = 'none';
-  body.style = 'height: 312px';
   createCardsAndshowCards();
   decideRandomOfWinner();
   
   let numberOfClickToCard = 0;
-  const losers = document.querySelectorAll('.card__looser');
+  const losers = document.querySelectorAll(`.${config.classNameOfLuserCards}`);
   losers.forEach((elem) => {
     elem.addEventListener('click', () => {
       while (true) {
         if (numberOfClickToCard % 2 == 0) {
-          elem.style.transform = 'rotateY(180deg)';
+          elem.className = config.classNameOfCardLooserRotated;
           numberOfClickToCard += 1;
           break;
         } else if (numberOfClickToCard % 2 == 1) {
@@ -62,11 +84,11 @@ const startGameAndDecideWinnerAndLoosers = () => {
     })
   })
       
-  const win = document.getElementById('win');
+  const win = document.getElementById(config.cardWin);
   const clickOnWinnerCard = () => {
     while (true) {
       if (numberOfClickToCard % 2 == 0) {
-        win.style.transform = 'rotateY(180deg)';
+        win.className = config.classNameOfCardWinnerRotated;
         numberOfClickToCard += 1;
         break;
        } else if (numberOfClickToCard % 2 == 1) {
@@ -80,35 +102,30 @@ const startGameAndDecideWinnerAndLoosers = () => {
 }
 button.addEventListener('click', startGameAndDecideWinnerAndLoosers);
 
-
-
-
-
 const createMainInHtml = () => {
   const main = document.createElement('main');
-  main.className = 'main';
   main.setAttribute('id', 'main');
   document.body.append(main);
 }
 
-const createCards = (number, className) => {
+const createCards = (number) => {
   for (let i = 0; i < number; i++) {
     const card = document.createElement('div');
-    card.className = `${className}`;
+    card.className = config.classNameOfCards;
     document.body>main.append(card);
   }
 }
 
-const addFrontAndBackSidesToCard = classNameOfCards => {
+const addFrontAndBackSidesToCards = () => {
   let numberOfEachCardInTurn = 0;
-  Array.from(document.querySelectorAll(classNameOfCards)).forEach(el => {
+  Array.from(document.querySelectorAll(`.${config.classNameOfAllCards}`)).forEach(el => {
     cards.push(el);
     const cardFront = document.createElement('div');
-    cardFront.className = 'card__front';
+    cardFront.className = config.classNameOfFrontSideForCard;
     document.body>main>cards[numberOfEachCardInTurn].append(cardFront);
 
     const cardBack = document.createElement('div');
-    cardBack.className = 'card__looser_back';
+    cardBack.className = config.classNameOfBackSideForCard;
     document.body>main>cards[numberOfEachCardInTurn].append(cardBack);
     numberOfEachCardInTurn++;
   });
@@ -116,15 +133,8 @@ const addFrontAndBackSidesToCard = classNameOfCards => {
 
 const decideRandomOfWinner = () => {
   const randomNumberOfCard = cards[Math.floor(Math.random() * (numberOfCard))];
-  const a = randomNumberOfCard.querySelector('.card__looser_back');
-  a.className = 'card__winner_back';
-  randomNumberOfCard.setAttribute('id', 'win');
-  
-  if (numberOfCard == 3) {
-    randomNumberOfCard.className = 'card__easy';
-  } else if (numberOfCard == 6) {
-     randomNumberOfCard.className = 'card__medium';
-  } else {
-    randomNumberOfCard.className = 'card__hard';
-  }
+  const a = randomNumberOfCard.querySelector(`.${config.classNameOfLoserBack}`);
+  a.className = config.classNameOfWinnerBack;
+  randomNumberOfCard.setAttribute('id', 'card-win');
+  randomNumberOfCard.className = config.classNameOfRandomWinnerCard;
 }
